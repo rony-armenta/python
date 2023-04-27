553804d681afe5ba2e29f2d312adc996a1f2c4ab
Rony Armenta <ronyarmenta38@gmail.com>
2023-04-27 11:18:19 -0700

    commit incial

---
+++ python.py
@@ -0,0 +1,21 @@
+# first_name = input('Type your name please: ')
+# last_name= input ('Type your last name please: ')
+# print(first_name + last_name)
+# print ('Hello ' + first_name + ' ' + last_name)
+
+# first_name='Rony'
+# last_name='Armenta'
+# print(first_name.upper() + ' ' + last_name.upper())
+
+# n=int(input("Type number 1:"))
+# y=int(input("Type number 2: "))
+# if n >= 10:
+#     print (n + y)
+# else:
+#     print(n*y)
+password=(input("Type new pass: "))
+if password == "ra1820":
+    print("Pass is correct")
+else:
+     print("Pass is false")
+    
